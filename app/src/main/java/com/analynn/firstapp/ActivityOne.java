88f8d4228148aef721e0b01443a4dbe8483c5b8e
ABuguina <analynnjane.buguina.iics@ust.edu.ph>
2017-09-30 01:09:27 +0800

    commit

---
+++ app/src/main/java/com/analynn/firstapp/ActivityOne.java
@@ -0,0 +1,22 @@
+package com.analynn.firstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.leo.firstapp.R;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+
+    public void nextActivity (View view) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+    }
+}
